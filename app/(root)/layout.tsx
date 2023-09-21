8a421da7a9e1149768f31634ad87ce48ac75a534
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-09-21 14:28:03 +0300

    add: topbar and leftbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
-import "./../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import { ClerkProvider } from "@clerk/nextjs";
+
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+import "./../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +24,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,5 @@
+function RightSidebar() {
+  return <h1>rightside</h1>;
+}
+
+export default RightSidebar;
