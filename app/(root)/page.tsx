fef6eff4d782b886e773d2e37b9a529dfa4c8b35
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-09-22 13:01:28 +0300

    add: fetch threads at home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
-import { UserButton } from "@clerk/nextjs";
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchThreads } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchThreads(1, 30);
+  const user = await currentUser();
+
+  console.log(result);
+
   return (
-    <div>
-      <h1 className="head-text text-left">Home</h1>
-    </div>
+    <>
+      <h1 className="text-left head-text">Home</h1>
+      <section className="flex flex-col gap-10 mt-9">
+        {result.threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.threads.map((thread) => (
+              <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id || ""}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
   );
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -40,3 +40,37 @@ export async function createThread({
     throw new Error(`Error creating thread; ${error.message}`);
   }
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of threads to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch the posts that have no parent
+  const threadsQuery = Thread.find({
+    parentId: { $in: [null, undefined] },
+  })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalThreadCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const threads = await threadsQuery.exec();
+
+  const isNext = totalThreadCount > skipAmount + threads.length;
+
+  return { threads, isNext };
+}
